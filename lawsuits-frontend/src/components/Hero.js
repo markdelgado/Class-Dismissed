802986abeb97b1d805811869ab2508662ea418b0
Mark Delgado <markdelgado.md@hotmail.com>
2024-11-22 07:34:57 -0500

    Added logo

--- lawsuits-frontend/src/components/Hero.js
+++ lawsuits-frontend/src/components/Hero.js
@@ -1,10 +1,12 @@
 import React from "react";
 import '../Hero.css'
+import Logo from '../images/logoClassDismissed.png'
+
 
 const Hero = () => {
     return(
         <div className="hero">   
-         <image src="./images/logoClassDismissed.png"></image>
+            <img src={Logo} alt="logo" width="30%" content="center" padding-left="15%"/>
             <p>Discover and stay informed about the latest class action lawsuits. From settlement details to claim deadlines, we’ve got you covered. Empower yourself with the information you need to claim your share and protect your rights—all in one place.</p>
     </div >
     )
